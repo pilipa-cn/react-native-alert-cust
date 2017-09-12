4759732eab4c7782754186a6c41fe64bdc5ab85e
zhuangzihao <zhuangzh@i-counting.cn>
2017-09-12 09:42:42 +0800

    ..

--- Example/App.js
+++ Example/App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
                 },
                 // color: "#EF0C35"
             }
-        ],{isSquare:true});
+        ]);
     }
 
     showSingleAlert() {
